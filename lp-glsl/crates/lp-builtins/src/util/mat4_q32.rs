06247c0190f29c2f20d10594891b6390838847c3
Yona Appletree <hypher@gmail.com>
2026-01-26 15:53:18 -0800

    lpc: mat2q32-mat3q32-mat4q32 - phase 3: mat4q32-implementation

---
+++ lp-glsl/crates/lp-builtins/src/util/mat4_q32.rs
@@ -0,0 +1,474 @@
+use core::ops::{Add, Div, Mul, Neg, Sub};
+
+use super::mat3_q32::Mat3Q32;
+use super::q32::Q32;
+use super::vec4_q32::Vec4Q32;
+
+/// 4x4 matrix for Q32 fixed-point arithmetic (GLSL-compatible, column-major storage)
+///
+/// Storage layout (column-major):
+/// [m00, m10, m20, m30, m01, m11, m21, m31, m02, m12, m22, m32, m03, m13, m23, m33]
+/// Where m[row][col] represents the element at row `row` and column `col`
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Mat4Q32 {
+    // Column-major storage: [col0, col1, col2, col3] where each column is [x, y, z, w]
+    // Storage: [m00, m10, m20, m30, m01, m11, m21, m31, m02, m12, m22, m32, m03, m13, m23, m33]
+    pub m: [Q32; 16],
+}
+
+impl Mat4Q32 {
+    /// Create a new matrix from 16 Q32 values (column-major order)
+    ///
+    /// Parameters are in column-major order:
+    /// m00, m10, m20, m30, m01, m11, m21, m31, m02, m12, m22, m32, m03, m13, m23, m33
+    #[allow(clippy::too_many_arguments)]
+    #[inline(always)]
+    pub const fn new(
+        m00: Q32,
+        m10: Q32,
+        m20: Q32,
+        m30: Q32,
+        m01: Q32,
+        m11: Q32,
+        m21: Q32,
+        m31: Q32,
+        m02: Q32,
+        m12: Q32,
+        m22: Q32,
+        m32: Q32,
+        m03: Q32,
+        m13: Q32,
+        m23: Q32,
+        m33: Q32,
+    ) -> Self {
+        Mat4Q32 {
+            m: [
+                m00, m10, m20, m30, m01, m11, m21, m31, m02, m12, m22, m32, m03, m13, m23, m33,
+            ],
+        }
+    }
+
+    /// Create a matrix from 16 f32 values (column-major order)
+    #[allow(clippy::too_many_arguments)]
+    #[inline(always)]
+    pub fn from_f32(
+        m00: f32,
+        m10: f32,
+        m20: f32,
+        m30: f32,
+        m01: f32,
+        m11: f32,
+        m21: f32,
+        m31: f32,
+        m02: f32,
+        m12: f32,
+        m22: f32,
+        m32: f32,
+        m03: f32,
+        m13: f32,
+        m23: f32,
+        m33: f32,
+    ) -> Self {
+        Mat4Q32::new(
+            Q32::from_f32(m00),
+            Q32::from_f32(m10),
+            Q32::from_f32(m20),
+            Q32::from_f32(m30),
+            Q32::from_f32(m01),
+            Q32::from_f32(m11),
+            Q32::from_f32(m21),
+            Q32::from_f32(m31),
+            Q32::from_f32(m02),
+            Q32::from_f32(m12),
+            Q32::from_f32(m22),
+            Q32::from_f32(m32),
+            Q32::from_f32(m03),
+            Q32::from_f32(m13),
+            Q32::from_f32(m23),
+            Q32::from_f32(m33),
+        )
+    }
+
+    /// Create a matrix from 4 Vec4Q32 columns
+    #[inline(always)]
+    pub fn from_vec4(col0: Vec4Q32, col1: Vec4Q32, col2: Vec4Q32, col3: Vec4Q32) -> Self {
+        Mat4Q32::new(
+            col0.x, col0.y, col0.z, col0.w, col1.x, col1.y, col1.z, col1.w, col2.x, col2.y, col2.z,
+            col2.w, col3.x, col3.y, col3.z, col3.w,
+        )
+    }
+
+    /// Create identity matrix
+    #[inline(always)]
+    pub const fn identity() -> Self {
+        Mat4Q32::new(
+            Q32::ONE,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ONE,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ONE,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ONE,
+        )
+    }
+
+    /// Create zero matrix
+    #[inline(always)]
+    pub const fn zero() -> Self {
+        Mat4Q32::new(
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+            Q32::ZERO,
+        )
+    }
+
+    /// Get element at row `row` and column `col`
+    #[inline(always)]
+    pub fn get(self, row: usize, col: usize) -> Q32 {
+        self.m[col * 4 + row]
+    }
+
+    /// Set element at row `row` and column `col`
+    #[inline(always)]
+    pub fn set(&mut self, row: usize, col: usize, value: Q32) {
+        self.m[col * 4 + row] = value;
+    }
+
+    /// Get column 0 as Vec4Q32
+    #[inline(always)]
+    pub fn col0(self) -> Vec4Q32 {
+        Vec4Q32::new(self.m[0], self.m[1], self.m[2], self.m[3])
+    }
+
+    /// Get column 1 as Vec4Q32
+    #[inline(always)]
+    pub fn col1(self) -> Vec4Q32 {
+        Vec4Q32::new(self.m[4], self.m[5], self.m[6], self.m[7])
+    }
+
+    /// Get column 2 as Vec4Q32
+    #[inline(always)]
+    pub fn col2(self) -> Vec4Q32 {
+        Vec4Q32::new(self.m[8], self.m[9], self.m[10], self.m[11])
+    }
+
+    /// Get column 3 as Vec4Q32
+    #[inline(always)]
+    pub fn col3(self) -> Vec4Q32 {
+        Vec4Q32::new(self.m[12], self.m[13], self.m[14], self.m[15])
+    }
+
+    /// Matrix-matrix multiplication
+    #[allow(clippy::should_implement_trait)]
+    #[inline(always)]
+    pub fn mul(self, rhs: Self) -> Self {
+        let a = self;
+        let b = rhs;
+        Mat4Q32::new(
+            // Row 0
+            a.m[0] * b.m[0] + a.m[4] * b.m[1] + a.m[8] * b.m[2] + a.m[12] * b.m[3],
+            a.m[1] * b.m[0] + a.m[5] * b.m[1] + a.m[9] * b.m[2] + a.m[13] * b.m[3],
+            a.m[2] * b.m[0] + a.m[6] * b.m[1] + a.m[10] * b.m[2] + a.m[14] * b.m[3],
+            a.m[3] * b.m[0] + a.m[7] * b.m[1] + a.m[11] * b.m[2] + a.m[15] * b.m[3],
+            // Row 1
+            a.m[0] * b.m[4] + a.m[4] * b.m[5] + a.m[8] * b.m[6] + a.m[12] * b.m[7],
+            a.m[1] * b.m[4] + a.m[5] * b.m[5] + a.m[9] * b.m[6] + a.m[13] * b.m[7],
+            a.m[2] * b.m[4] + a.m[6] * b.m[5] + a.m[10] * b.m[6] + a.m[14] * b.m[7],
+            a.m[3] * b.m[4] + a.m[7] * b.m[5] + a.m[11] * b.m[6] + a.m[15] * b.m[7],
+            // Row 2
+            a.m[0] * b.m[8] + a.m[4] * b.m[9] + a.m[8] * b.m[10] + a.m[12] * b.m[11],
+            a.m[1] * b.m[8] + a.m[5] * b.m[9] + a.m[9] * b.m[10] + a.m[13] * b.m[11],
+            a.m[2] * b.m[8] + a.m[6] * b.m[9] + a.m[10] * b.m[10] + a.m[14] * b.m[11],
+            a.m[3] * b.m[8] + a.m[7] * b.m[9] + a.m[11] * b.m[10] + a.m[15] * b.m[11],
+            // Row 3
+            a.m[0] * b.m[12] + a.m[4] * b.m[13] + a.m[8] * b.m[14] + a.m[12] * b.m[15],
+            a.m[1] * b.m[12] + a.m[5] * b.m[13] + a.m[9] * b.m[14] + a.m[13] * b.m[15],
+            a.m[2] * b.m[12] + a.m[6] * b.m[13] + a.m[10] * b.m[14] + a.m[14] * b.m[15],
+            a.m[3] * b.m[12] + a.m[7] * b.m[13] + a.m[11] * b.m[14] + a.m[15] * b.m[15],
+        )
+    }
+
+    /// Matrix-vector multiplication (mat4 * vec4)
+    #[inline(always)]
+    pub fn mul_vec4(self, v: Vec4Q32) -> Vec4Q32 {
+        Vec4Q32::new(
+            self.m[0] * v.x + self.m[4] * v.y + self.m[8] * v.z + self.m[12] * v.w,
+            self.m[1] * v.x + self.m[5] * v.y + self.m[9] * v.z + self.m[13] * v.w,
+            self.m[2] * v.x + self.m[6] * v.y + self.m[10] * v.z + self.m[14] * v.w,
+            self.m[3] * v.x + self.m[7] * v.y + self.m[11] * v.z + self.m[15] * v.w,
+        )
+    }
+
+    /// Transpose matrix
+    #[inline(always)]
+    pub fn transpose(self) -> Self {
+        Mat4Q32::new(
+            self.m[0], self.m[4], self.m[8], self.m[12], self.m[1], self.m[5], self.m[9],
+            self.m[13], self.m[2], self.m[6], self.m[10], self.m[14], self.m[3], self.m[7],
+            self.m[11], self.m[15],
+        )
+    }
+
+    /// Calculate determinant using Laplace expansion
+    #[inline(always)]
+    pub fn determinant(self) -> Q32 {
+        let m = &self.m;
+        // Laplace expansion along first row
+        let a = m[0]
+            * Mat3Q32::new(m[5], m[6], m[7], m[9], m[10], m[11], m[13], m[14], m[15]).determinant();
+        let b = m[4]
+            * Mat3Q32::new(m[1], m[2], m[3], m[9], m[10], m[11], m[13], m[14], m[15]).determinant();
+        let c = m[8]
+            * Mat3Q32::new(m[1], m[2], m[3], m[5], m[6], m[7], m[13], m[14], m[15]).determinant();
+        let d = m[12]
+            * Mat3Q32::new(m[1], m[2], m[3], m[5], m[6], m[7], m[9], m[10], m[11]).determinant();
+        a - b + c - d
+    }
+
+    /// Calculate inverse matrix
+    ///
+    /// Returns None if matrix is singular (determinant is zero)
+    #[inline(always)]
+    pub fn inverse(self) -> Option<Self> {
+        let det = self.determinant();
+        if det.to_fixed() == 0 {
+            return None;
+        }
+
+        let m = &self.m;
+        // Calculate cofactor matrix (transposed for adjugate)
+        // Using 3x3 determinants for each cofactor
+        let c00 =
+            Mat3Q32::new(m[5], m[6], m[7], m[9], m[10], m[11], m[13], m[14], m[15]).determinant();
+        let c01 =
+            -Mat3Q32::new(m[1], m[2], m[3], m[9], m[10], m[11], m[13], m[14], m[15]).determinant();
+        let c02 =
+            Mat3Q32::new(m[1], m[2], m[3], m[5], m[6], m[7], m[13], m[14], m[15]).determinant();
+        let c03 =
+            -Mat3Q32::new(m[1], m[2], m[3], m[5], m[6], m[7], m[9], m[10], m[11]).determinant();
+
+        let c10 =
+            -Mat3Q32::new(m[4], m[6], m[7], m[8], m[10], m[11], m[12], m[14], m[15]).determinant();
+        let c11 =
+            Mat3Q32::new(m[0], m[2], m[3], m[8], m[10], m[11], m[12], m[14], m[15]).determinant();
+        let c12 =
+            -Mat3Q32::new(m[0], m[2], m[3], m[4], m[6], m[7], m[12], m[14], m[15]).determinant();
+        let c13 =
+            Mat3Q32::new(m[0], m[2], m[3], m[4], m[6], m[7], m[8], m[10], m[11]).determinant();
+
+        let c20 =
+            Mat3Q32::new(m[4], m[5], m[7], m[8], m[9], m[11], m[12], m[13], m[15]).determinant();
+        let c21 =
+            -Mat3Q32::new(m[0], m[1], m[3], m[8], m[9], m[11], m[12], m[13], m[15]).determinant();
+        let c22 =
+            Mat3Q32::new(m[0], m[1], m[3], m[4], m[5], m[7], m[12], m[13], m[15]).determinant();
+        let c23 =
+            -Mat3Q32::new(m[0], m[1], m[3], m[4], m[5], m[7], m[8], m[9], m[11]).determinant();
+
+        let c30 =
+            -Mat3Q32::new(m[4], m[5], m[6], m[8], m[9], m[10], m[12], m[13], m[14]).determinant();
+        let c31 =
+            Mat3Q32::new(m[0], m[1], m[2], m[8], m[9], m[10], m[12], m[13], m[14]).determinant();
+        let c32 =
+            -Mat3Q32::new(m[0], m[1], m[2], m[4], m[5], m[6], m[12], m[13], m[14]).determinant();
+        let c33 = Mat3Q32::new(m[0], m[1], m[2], m[4], m[5], m[6], m[8], m[9], m[10]).determinant();
+
+        // Adjugate matrix (transpose of cofactor) divided by determinant
+        let inv_det = Q32::ONE / det;
+        Some(Mat4Q32::new(
+            c00 * inv_det,
+            c10 * inv_det,
+            c20 * inv_det,
+            c30 * inv_det,
+            c01 * inv_det,
+            c11 * inv_det,
+            c21 * inv_det,
+            c31 * inv_det,
+            c02 * inv_det,
+            c12 * inv_det,
+            c22 * inv_det,
+            c32 * inv_det,
+            c03 * inv_det,
+            c13 * inv_det,
+            c23 * inv_det,
+            c33 * inv_det,
+        ))
+    }
+}
+
+// Matrix + Matrix
+impl Add for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn add(self, rhs: Self) -> Self {
+        Mat4Q32::new(
+            self.m[0] + rhs.m[0],
+            self.m[1] + rhs.m[1],
+            self.m[2] + rhs.m[2],
+            self.m[3] + rhs.m[3],
+            self.m[4] + rhs.m[4],
+            self.m[5] + rhs.m[5],
+            self.m[6] + rhs.m[6],
+            self.m[7] + rhs.m[7],
+            self.m[8] + rhs.m[8],
+            self.m[9] + rhs.m[9],
+            self.m[10] + rhs.m[10],
+            self.m[11] + rhs.m[11],
+            self.m[12] + rhs.m[12],
+            self.m[13] + rhs.m[13],
+            self.m[14] + rhs.m[14],
+            self.m[15] + rhs.m[15],
+        )
+    }
+}
+
+// Matrix - Matrix
+impl Sub for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn sub(self, rhs: Self) -> Self {
+        Mat4Q32::new(
+            self.m[0] - rhs.m[0],
+            self.m[1] - rhs.m[1],
+            self.m[2] - rhs.m[2],
+            self.m[3] - rhs.m[3],
+            self.m[4] - rhs.m[4],
+            self.m[5] - rhs.m[5],
+            self.m[6] - rhs.m[6],
+            self.m[7] - rhs.m[7],
+            self.m[8] - rhs.m[8],
+            self.m[9] - rhs.m[9],
+            self.m[10] - rhs.m[10],
+            self.m[11] - rhs.m[11],
+            self.m[12] - rhs.m[12],
+            self.m[13] - rhs.m[13],
+            self.m[14] - rhs.m[14],
+            self.m[15] - rhs.m[15],
+        )
+    }
+}
+
+// Matrix * Matrix (matrix multiplication)
+impl Mul for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn mul(self, rhs: Self) -> Self {
+        self.mul(rhs)
+    }
+}
+
+// Matrix * Vec4 (matrix-vector multiplication)
+impl Mul<Vec4Q32> for Mat4Q32 {
+    type Output = Vec4Q32;
+
+    #[inline(always)]
+    fn mul(self, rhs: Vec4Q32) -> Vec4Q32 {
+        self.mul_vec4(rhs)
+    }
+}
+
+// Matrix * Scalar
+impl Mul<Q32> for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn mul(self, rhs: Q32) -> Self {
+        Mat4Q32::new(
+            self.m[0] * rhs,
+            self.m[1] * rhs,
+            self.m[2] * rhs,
+            self.m[3] * rhs,
+            self.m[4] * rhs,
+            self.m[5] * rhs,
+            self.m[6] * rhs,
+            self.m[7] * rhs,
+            self.m[8] * rhs,
+            self.m[9] * rhs,
+            self.m[10] * rhs,
+            self.m[11] * rhs,
+            self.m[12] * rhs,
+            self.m[13] * rhs,
+            self.m[14] * rhs,
+            self.m[15] * rhs,
+        )
+    }
+}
+
+// Matrix / Scalar
+impl Div<Q32> for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn div(self, rhs: Q32) -> Self {
+        Mat4Q32::new(
+            self.m[0] / rhs,
+            self.m[1] / rhs,
+            self.m[2] / rhs,
+            self.m[3] / rhs,
+            self.m[4] / rhs,
+            self.m[5] / rhs,
+            self.m[6] / rhs,
+            self.m[7] / rhs,
+            self.m[8] / rhs,
+            self.m[9] / rhs,
+            self.m[10] / rhs,
+            self.m[11] / rhs,
+            self.m[12] / rhs,
+            self.m[13] / rhs,
+            self.m[14] / rhs,
+            self.m[15] / rhs,
+        )
+    }
+}
+
+// -Matrix
+impl Neg for Mat4Q32 {
+    type Output = Self;
+
+    #[inline(always)]
+    fn neg(self) -> Self {
+        Mat4Q32::new(
+            -self.m[0],
+            -self.m[1],
+            -self.m[2],
+            -self.m[3],
+            -self.m[4],
+            -self.m[5],
+            -self.m[6],
+            -self.m[7],
+            -self.m[8],
+            -self.m[9],
+            -self.m[10],
+            -self.m[11],
+            -self.m[12],
+            -self.m[13],
+            -self.m[14],
+            -self.m[15],
+        )
+    }
+}
--- lp-glsl/crates/lp-builtins/src/util/mod.rs
+++ lp-glsl/crates/lp-builtins/src/util/mod.rs
@@ -1,5 +1,6 @@
 pub mod mat2_q32;
 pub mod mat3_q32;
+pub mod mat4_q32;
 pub mod q32;
 #[cfg(test)]
 pub mod test_helpers;
@@ -9,6 +10,7 @@ pub mod vec4_q32;
 
 pub use mat2_q32::Mat2Q32;
 pub use mat3_q32::Mat3Q32;
+pub use mat4_q32::Mat4Q32;
 pub use vec2_q32::Vec2Q32;
 pub use vec3_q32::Vec3Q32;
 pub use vec4_q32::Vec4Q32;
